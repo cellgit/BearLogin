dbec095504c170f9508a890f6b4d435f8c929b49
liuhongli <>
2024-12-09 16:47:51 +0800

    [t0]xx

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["BearLogin"]),
     ],
     dependencies: [
-        .package(path: "../BearBasic"),
-//        .package(url: "https://github.com/cellgit/BearBasic.git", .upToNextMajor(from: "0.0.1")),
+//        .package(path: "../BearBasic"),
+        .package(url: "https://github.com/cellgit/BearBasic.git", .upToNextMajor(from: "0.0.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
